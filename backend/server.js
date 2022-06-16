136d80d1ac8030c4c7604fe8e71f8657f3d46cd0
ViktoriTanos <99451694+Viktoriaset@users.noreply.github.com>
2022-06-16 17:16:16 +1000

    Create lobbi and register system:

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,75 @@
 const http = require('http');
 const express = require('express');
+const {v4: uuidv4} = require('uuid');
 const socketio = require('socket.io');
 const {Server} = require("socket.io");
 const bodyParser = require('body-parser')
 const {colours} = require("nodemon/lib/config/defaults");
+const Console = require("console");
 
-class Player{
+class Client{
     constructor(name) {
         this.name = name;
+        this.lobbiId = -1;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getLobbiId(){
+        return this.name;
+    }
+
+    setLobbiId(value){
+        this.lobbiId = value;
     }
 }
 
-var players = [];
+class Lobbi{
+    constructor(client1, client2, id) {
+        this.id = id;
+        this.clients = [];
+        this.clients.push(client1.getName());
+        this.clients.push(client2.getName());
+    }
+
+    getId(){
+        return this.id;
+    }
+
+    getClient(index){
+        if (!Number.isInteger(index)){
+            Console.log("Error don't have client with index " + index + " in this lobbi")
+            return -1;
+        } else if(index > -1 && index < this.clients.length) {
+            Console.log("Error don't have client with index " + index + " in this lobbi")
+            return -1;
+        } else {
+            return this.clients[index];
+        }
+    }
+}
+
+
+var Players = [];
+var Lobbies = [];
+
+function chekEmptyLobbi(){
+    if (Players.length <= 1){
+        return;
+    }
+    if (Players[Players.length - 2].lobbiId === -1){
+        createLobbi(Players[Players.length - 2], Players[Players.length - 1]);
+    }
+}
+
+function createLobbi(client1, client2){
+    var id = uuidv4();
+    client1.setLobbiId(id);
+    client2.setLobbiId(id);
+    Lobbies.push(new Lobbi(client1, client2, id));
+}
 
 
 const app = express();
@@ -30,7 +88,7 @@ console.log(io.path());
 io.on('connection', (sock) =>{
     sock.on('Registred', (name) =>{
         console.log(name);
-        players.push(new Player(name));
+        players.push(new Client(name));
     })
 })
 
@@ -43,12 +101,16 @@ app.get('/', (req, res) =>{
 })
 
 app.post('/', urlencodedParser, (req, res) => {
-    players.push(new Player(req.body.name));
+    Players.push(new Client(req.body.name));
+    chekEmptyLobbi();
     res.sendFile("C:\\Users\\виктор\\Desktop\\webgame-weblab\\public\\index.html")
 })
 
 app.get('/playrs', (req, res) =>{
-    res.send(players);
+    res.send(Players);
+})
+app.get('/lobbi', (req, res) =>{
+    res.send(Lobbies);
 })
 
 
